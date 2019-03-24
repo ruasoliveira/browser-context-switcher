36216d39fb3b731afd12a9a8a564e3a5cdb1aee6
Rodrigo Ruas Oliveira <rodrigoruasoliveira@gmail.com>
2019-03-24 15:56:11 -0300

    Extension: added handlers for tab creation, removal, update - add: onRemoved, onCreated, and onUpdated handlers, triggering displayTabsURLs - add: check if tab is going to be removed and ignore it (tab is not removed instantly, which means displayTabsURLs would show it) - filter out 'moz-extension://' URLs

--- firefox/selection/debug.js
+++ firefox/selection/debug.js
@@ -1,14 +1,35 @@
+var G_d_removed_tabs = {};
 
 /**
  * Displays a list of all open URLs, both in the console and the popup page
  */
 function displayTabsURLs( l_tabs ) {
+  // clean up the div (remove previous values)
+  var nd_selection = document.getElementById("selection-content");
+  while (nd_selection.firstChild) {
+    nd_selection.removeChild( nd_selection.firstChild );
+  }
 
   var l_urls = document.createElement('ul');
+  var s_date = new Date().toLocaleString();
+  var o_item = document.createElement('li');
+  o_item.appendChild( document.createTextNode( s_date ) );
+  l_urls.appendChild( o_item );
   for (let o_tab of l_tabs) {
-    console.log(`${o_tab.url}`);
 
-    if (o_tab.incognito) { // skip private browsing tabs
+    // skip private browsing tabs
+    if (o_tab.incognito) {
+      continue;
+    }
+
+    // skip tabs scheduled to be removed
+    var idx_composed_id = 'tab'+o_tab.id+'win'+o_tab.windowId;
+    if (idx_composed_id in G_d_removed_tabs) {
+      continue;
+    }
+
+    // skip extension tabs (desirable?)
+    if ( o_tab.url.includes("moz-extension://") ) {
       continue;
     }
 
@@ -30,8 +51,30 @@ function displayTabsURLs( l_tabs ) {
     o_item.appendChild( document.createTextNode(s_url_text) );
     l_urls.appendChild( o_item );
   }
+  nd_selection.appendChild( l_urls );
 
-  document.getElementById("selection-content").appendChild( l_urls );
+  G_d_removed_tabs = {};
+}
+
+function handleRemoved( tabId, removeInfo ) {
+  var idx_composed_id = 'tab'+tabId+'win'+removeInfo.windowId
+  G_d_removed_tabs[idx_composed_id] = true;
+
+  if ( !removeInfo.isWindowClosing ) {
+    var prm_l_tabs = browser.tabs.query({});
+    prm_l_tabs.then( displayTabsURLs, reportError);
+  }
+
+}
+
+function handleCreated(tab) {
+  var prm_l_tabs = browser.tabs.query({});
+  prm_l_tabs.then( displayTabsURLs, reportError);
+}
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+  var prm_l_tabs = browser.tabs.query({});
+  prm_l_tabs.then( displayTabsURLs, reportError);
 }
 
 /**
@@ -52,3 +95,6 @@ var prm_l_tabs = browser.tabs.query({});
 
 prm_l_tabs.then( displayTabsURLs, reportError)
 
+browser.tabs.onRemoved.addListener( handleRemoved );
+browser.tabs.onCreated.addListener( handleCreated );
+browser.tabs.onUpdated.addListener( handleUpdated );
